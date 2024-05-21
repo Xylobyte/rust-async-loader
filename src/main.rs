e79f5bfa58617038331930c64ef476a51f287469
Nantsa Montillet <nantsamontillet@xylobyte.com>
2024-05-21 17:12:56 +0200

    Change everything

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::ops::ControlFlow::Break;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{self, Sender, Receiver};
 use std::thread;
@@ -9,105 +8,101 @@ use rand::Rng;
 struct Task {
     id: u32,
     progress: Arc<Mutex<u8>>,
+    progress_tx: Sender<SchedulerMessage>,
 }
 
 impl Task {
-    fn new(id: u32) -> Self {
+    fn new(id: u32, progress_tx: Sender<SchedulerMessage>) -> Self {
         Task {
             id,
             progress: Arc::new(Mutex::new(0)),
+            progress_tx,
         }
     }
 
     fn start(&self) {
         let progress = Arc::clone(&self.progress);
         let task_id = self.id;
+        let progress_tx = self.progress_tx.clone();
         thread::spawn(move || {
             let mut rng = rand::thread_rng();
             for i in 0..=100 {
                 thread::sleep(Duration::from_millis(rng.gen_range(10..100)));
                 let mut prog = progress.lock().unwrap();
                 *prog = i;
+                progress_tx.send(SchedulerMessage::ProgressUpdate(task_id, i)).unwrap();
             }
             println!("Task {} finished.", task_id);
+            progress_tx.send(SchedulerMessage::TaskFinished(task_id)).unwrap();
         });
     }
 }
 
 enum SchedulerMessage {
-    NewTask(u32),
+    NewTask(Sender<u32>),
     TaskFinished(u32),
+    GetProgress(u32, Sender<u8>),
+    ProgressUpdate(u32, u8),
     Stop,
 }
 
 fn main() {
-    // Channel for communicating with the scheduler thread
-    let (tx, rx): (Sender<SchedulerMessage>, Receiver<SchedulerMessage>) = mpsc::channel();
-    let (progress_tx, progress_rx): (Sender<(u32, Arc<Mutex<u8>>)>, Receiver<(u32, Arc<Mutex<u8>>)>) = mpsc::channel();
+    let (scheduler_tx, scheduler_rx): (Sender<SchedulerMessage>, Receiver<SchedulerMessage>) = mpsc::channel();
 
-    // Scheduler thread
-    let scheduler_tx = tx.clone();
-    let scheduler_progress_tx = progress_tx.clone();
-    let scheduler_handle = thread::spawn(move || {
-        let mut task_id_counter = 0;
-        let mut threads = vec![];
+    let scheduler_tx_cone = scheduler_tx.clone();
+
+    let scheduler = thread::spawn(move || {
+        let mut actual_id = 0;
+        let mut tasks_ids: Vec<u32> = Vec::new();
+        let mut progress_map: std::collections::HashMap<u32, u8> = std::collections::HashMap::new();
 
         loop {
-            //println!("Thread count: {}", threads.len());
-            match rx.recv() {
-                Ok(SchedulerMessage::NewTask(task_id)) => {
-                    if threads.len() < 10 {
-                        let task = Task::new(task_id);
-                        scheduler_progress_tx.send((task_id, Arc::clone(&task.progress))).unwrap();
-                        task.start();
-                        threads.push(task_id);
-                        task_id_counter += 1;
-                        println!("Task {} started.", task_id);
-                    } else {
-                        println!("Maximum number of threads reached. Task {} will be delayed.", task_id);
-                        // Add a delay to simulate distribution to other threads
-                        thread::sleep(Duration::from_secs(1));
-                        scheduler_tx.send(SchedulerMessage::NewTask(task_id)).unwrap();
+            match scheduler_rx.recv() {
+                Ok(SchedulerMessage::NewTask(response_tx)) => {
+                    tasks_ids.push(actual_id);
+                    let task = Task::new(actual_id, scheduler_tx.clone());
+                    task.start();
+                    response_tx.send(actual_id).unwrap();
+                    actual_id += 1;
+                }
+                Ok(SchedulerMessage::TaskFinished(id)) => {
+                    tasks_ids.retain(|&x| x != id);
+                    println!("--- Task {} finished.", id);
+                }
+                Ok(SchedulerMessage::GetProgress(id, response_tx)) => {
+                    if let Some(&progress) = progress_map.get(&id) {
+                        response_tx.send(progress).unwrap();
                     }
-                },
-                Ok(SchedulerMessage::TaskFinished(task_id)) => {
-                    let index = threads.iter().position(|&x| x == task_id).unwrap();
-                    threads.remove(index);
-                    println!("Task {} finished !", task_id);
-                },
+                }
+                Ok(SchedulerMessage::ProgressUpdate(id, progress)) => {
+                    progress_map.insert(id, progress);
+                }
                 Ok(SchedulerMessage::Stop) => {
-                    println!("Scheduler stopping...");
+                    println!("--- All tasks finished.");
                     break;
-                },
-                Err(_) => break,
+                }
+                Err(_) => {
+                    println!("--- Scheduler error.");
+                    break;
+                }
             }
         }
     });
 
-    // Wait for the scheduler thread to start
-    thread::sleep(Duration::from_secs(2));
+    let (new_task_tx, new_task_rx) = mpsc::channel();
+    scheduler_tx_cone.send(SchedulerMessage::NewTask(new_task_tx)).unwrap();
+    let new_task_id = new_task_rx.recv().unwrap();
+    println!("New task created with ID: {}", new_task_id);
 
-    // Create 200 tasks
-    for i in 1..=200 {
-        tx.send(SchedulerMessage::NewTask(i)).unwrap();
+    thread::sleep(Duration::from_millis(500));
+
+    let (progress_query_tx, progress_query_rx) = mpsc::channel();
+    scheduler_tx_cone.send(SchedulerMessage::GetProgress(new_task_id, progress_query_tx)).unwrap();
+    match progress_query_rx.recv() {
+        Ok(progress) => println!("Progress of task {}: {}%", new_task_id, progress),
+        Err(_) => println!("Failed to get progress of task {}", new_task_id),
     }
 
-    // Main thread polling the task's progress
-    loop {
-        thread::sleep(Duration::from_millis(500));
-        if let Ok((task_id, progress)) = progress_rx.try_recv() {
-            let progress = *progress.lock().unwrap();
-            println!("Task {} progress: {}%", task_id, progress);
-            println!("Yolo");
-            if progress >= 100 {
-                tx.send(SchedulerMessage::TaskFinished(task_id)).unwrap();
-            }
-        }
-    }
-
-    // Send a stop message to the scheduler
-    tx.send(SchedulerMessage::Stop).unwrap();
-
-    // Wait for the scheduler thread to finish
-    scheduler_handle.join().unwrap();
+    scheduler_tx_cone.send(SchedulerMessage::Stop).unwrap();
+    scheduler.join().unwrap();
 }
